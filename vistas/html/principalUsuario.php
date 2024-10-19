b9c106115f11517a6f07055db8e7272c729af469
Chidyfnfd <quinterozambranojuandiego@gmail.com>
2024-10-19 14:21:40 -0500

    Opcion, Crear cliente 50% aprox

--- vistas/html/principalUsuario.php
+++ vistas/html/principalUsuario.php
@@ -43,7 +43,7 @@
     <header class="header_section">
       <div class="container">
         <nav class="navbar navbar-expand-lg custom_nav-container ">
-          <a class="navbar-brand" href="index.html">
+          <a class="navbar-brand" href="index.php?accion=principalUsuario">
             <span>
               ChampiLoco
             </span>
@@ -57,7 +57,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav  mx-auto ">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php?accion=principalUsuario">Inicio <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="index.php?accion=productos">Productos</a>
